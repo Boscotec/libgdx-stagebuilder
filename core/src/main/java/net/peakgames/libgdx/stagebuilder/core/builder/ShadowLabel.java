863ed600ceccd293ca57f29bbd27b643b35981f4
sercangedik <sercan@peakgames.net>
2017-02-14 15:18:12 +0300

    set shadow pos

--- core/src/main/java/net/peakgames/libgdx/stagebuilder/core/builder/ShadowLabel.java
+++ core/src/main/java/net/peakgames/libgdx/stagebuilder/core/builder/ShadowLabel.java
@@ -8,24 +8,43 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 public class ShadowLabel extends Label {
 
     private Label shadowLabel;
+    private static final float shiftX = 2;
+    private static final float shiftY = 2;
+    private static float resolutionMult = 1;
 
     public ShadowLabel(CharSequence text, LabelStyle labelStyle, String shadowColorName) {
         this(text, labelStyle, Color.valueOf(shadowColorName));
+        repositionShadowLabel();
+    }
+
+    public ShadowLabel(CharSequence text, LabelStyle labelStyle, Color color, float resolutionMult) {
+        this(text, labelStyle, color);
+        this.resolutionMult = resolutionMult;
+        repositionShadowLabel();
+    }
+
+    public ShadowLabel(CharSequence text, LabelStyle labelStyle, String shadowColorName, float resolutionMult) {
+        this(text, labelStyle, Color.valueOf(shadowColorName));
+        this.resolutionMult = resolutionMult;
+        repositionShadowLabel();
+    }
+
+    private void repositionShadowLabel() {
+        float shadowX = getX() + (shiftX * resolutionMult);
+        float shadowY = getY() - (shiftY * resolutionMult);
+        shadowLabel.setPosition(shadowX, shadowY);
     }
 
     public ShadowLabel(CharSequence text, LabelStyle labelStyle, Color shadowColor) {
         super(text,labelStyle);
         LabelStyle shadowStyle = new LabelStyle(labelStyle.font, shadowColor);
         shadowLabel = new Label(text, shadowStyle);
-        float shadowX = getX()+6;
-        float shadowY = getY()-6;
-        shadowLabel.setPosition(shadowX, shadowY);
     }
 
     @Override
     public void setZIndex(int index) {
         super.setZIndex(index);
-        shadowLabel.setZIndex(index+100);
+        shadowLabel.setZIndex(index + 100);
     }
 
     @Override
@@ -58,14 +77,14 @@ public class ShadowLabel extends Label {
 
     @Override
     public void setPosition(float x, float y) {
-        shadowLabel.setPosition(x+1, y-1);
         super.setPosition(x, y);
+        shadowLabel.setPosition(x + (shiftX * resolutionMult), y - (shiftY * resolutionMult));
     }
 
     @Override
     public void setBounds(float x, float y, float width, float height) {
-        shadowLabel.setBounds(x+1, y-1, width, height);
         super.setBounds(x, y, width, height);
+        shadowLabel.setBounds(x + (shiftX * resolutionMult), y - (shiftY * resolutionMult), width, height);
     }
 
     public Label getShadowLabel() {
@@ -92,13 +111,13 @@ public class ShadowLabel extends Label {
     @Override
     public void setX(float x) {
         super.setX(x);
-        shadowLabel.setX(x+1);
+        shadowLabel.setX(x + (shiftX * resolutionMult));
     }
 
     @Override
     public void setY(float y) {
         super.setY(y);
-        shadowLabel.setY(y-1);
+        shadowLabel.setY(y - (shiftY * resolutionMult));
     }
 
     @Override
@@ -119,5 +138,27 @@ public class ShadowLabel extends Label {
         if(shadowLabel != null){
             shadowLabel.setSize(width, height);
         }
+    }
+
+    @Override
+    public void setWidth(float width) {
+        super.setWidth(width);
+        shadowLabel.setWidth(width);
+    }
+
+    @Override
+    public void setHeight(float height) {
+        super.setHeight(height);
+        shadowLabel.setHeight(height);
+    }
+
+    @Override
+    public float getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public float getHeight() {
+        return super.getHeight();
     }
 }
